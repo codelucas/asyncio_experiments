26fb8ac2d4d32cf3fdf2b1a6e6c39151b410119f
Lucas Ou-Yang <lucas@instagram.com>
2016-08-14 18:37:56 -0700

    Init commit, push all code written offline!

---
+++ README.rst
@@ -0,0 +1,52 @@
+**Motivation**:
+
+I'm curious on the relationship between the # of HTTP `Range requests` to download latency.
+Is it more efficient to download a huge file, say an image, in one request?  Or to split it up among 5 requests? How about 10? *At what threshold is it not worth it to continue splitting the request up?*
+
+**Results**:
+
+I ran the code myself with 10 iterations on [this 10MB web resource](https://upload.wikimedia.org/wikipedia/commons/5/56/The_Very_Large_Array%2C_New_Mexico.jpg) testing split sizes
+1, 4, 10, 16, 25, 50, 100, 200 and found that:
+```
+200 request splits resulted in average 5.8094 sec over 10 iterations ; 55,437 bytes per split
+100 request splits resulted in average 5.9908 sec over 10 iterations ; 110,875 bytes per split
+50 request splits resulted in average 6.5629 sec over 10 iterations ; 221,750 bytes per split
+25 request splits resulted in average 7.2891 sec over 10 iterations ; 443,500 bytes per split
+16 request splits resulted in average 8.1770 sec over 10 iterations ; 692,969 bytes per split
+10 request splits resulted in average 8.6593 sec over 10 iterations ; 1,108,751 bytes per split
+4 request splits resulted in average 11.6698 sec over 10 iterations ; 2,771,878 bytes per split
+1 request splits resulted in average 14.7066 sec over 10 iterations ; 11,087,513 bytes per split
+```
+
+Interesting! Even though our data sample size wasn't massive we can already see patterns:
+- More splits --> smaller payload per request --> faster concurrent downloading via asyncio
+- Going from 100 -> 200 splits seemed to have largely depreciating returns in latency! Thus, 110KB -> 55KB per request isn't that big of a saving (200ms) and perhaps the sweet spot is somewhere neat 50kb per request.
+
+**Try it yourself**:
+
+This code uses python3's new asyncio features; *You must run this code with python 3.5+!*
+Feel free to toggle the `` split sizes, and `` variables to get your own experiment going :) Don't forget to install aiohttp
+```
+virtualenv asyncio-experiments
+cd asyncio-experiments
+source bin/activate
+git clone https://github.com/codelucas/asyncio_experiments.git
+cd asyncio_experiments
+pip3 install aiohttp
+python3.5 main.py
+```
+
+**Caveats**:
+
+- This experiment is kinda undeveloped, there are still many variables that matter but are not measured, such as:
+   - The location of the server hosting the file, I just picked an arbitrary large img hosted on wikipedia
+   - Your internet connection
+   - Implementation of webserver hosting the file
+   - I wanted to use a more granular set of split sizes but didn't for the sake of time
+   - Any split size above 100 caused wikipedia's server to give me error response codes for too many sockets
+- If a site doesn't accept range requests, this api will fail with a printed error message
+- To not put burden on targeted sites, I set a sleep timer to 3 seconds between every iteration
+   - Try using this api to just target big websites that can probably handle the load
+   - You might get rate limited depending on the site hosting the url
+
+- By Lucas Ou-Yang, *lucasyangpersonal@gmail.com*
---
+++ main.py
@@ -0,0 +1,110 @@
+# encoding=utf-8
+
+# Lucas Ou-Yang, 2016
+
+import asyncio
+import itertools
+import time
+
+import aiohttp
+
+
+NUM_ITERATIONS = 1
+VERBOSE = True
+
+class Timer(object):
+    def __enter__(self):
+        self.start = time.time()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.time()
+        self.elapsed = self.end - self.start
+
+
+async def inspect_size(url):
+    """Returns an integer content length if the url is range-query-able --> a candidate
+    for this experiment, otherwise returns None
+    """
+    async with aiohttp.head(url) as resp:
+        accepted_ranges = resp.headers['Accept-Ranges']
+        if accepted_ranges and accepted_ranges.lower() == 'bytes':
+            content_length = resp.headers['Content-Length']
+            if content_length:
+                return int(content_length)
+    return None
+
+
+async def range_get(url, start, end):
+    http_headers = {
+        'Range': 'bytes={}-{}'.format(start, end - 1 if end else '')
+    }
+    if VERBOSE:
+        print('Performing range-get on bytes: %s-%s' % (str(start), str(end)))
+    async with aiohttp.get(url, headers=http_headers) as resp:
+        assert resp.status // 100 == 2
+        return await resp.read()
+
+
+async def download(url, num_splits):
+    content_length = await inspect_size(url)  # http.head isn't considered / timed in experiment
+    if content_length:
+        size_request = content_length // num_splits
+        ranges = list(range(0, content_length, size_request))
+        if VERBOSE:
+            print('Performing %s asyncio requests on a url with content-length %s bytes'
+                  % (str(num_splits), str(content_length)))
+            print('Each split will be %s bytes' % str(size_request))
+        with Timer() as t:
+            tasks, _ = await asyncio.wait([
+                range_get(url, start, end)
+                for start, end in itertools.zip_longest(ranges, ranges[1:], fillvalue="")
+            ])
+        assert len(tasks) == len(ranges)  # this assertions indicates no pending futures
+        if VERBOSE:
+            print('Function download(num_splits=%d) took %2.4f sec.' %
+                  (num_splits, t.elapsed))
+        return (num_splits, t.elapsed)
+    else:
+        print('URL: %s does not accept range GET queries, not candidate for this '
+              'experiment')
+    return
+
+
+async def download_many(url, splits):
+    tally = {}
+    for i in range(NUM_ITERATIONS):
+        print('Starting iteration %d' % (i + 1))
+        tasks, pending = await asyncio.wait([
+            download(url, split) for split in splits
+        ])
+        assert len(pending) == 0
+        current_tally = {}
+        for task in tasks:
+            _splits, _elapsed = task.result()
+            current_tally[_splits] = _elapsed
+        tally[i + 1] = current_tally
+        await asyncio.sleep(3)  # Give the website a break!
+    return tally
+
+
+if __name__ == '__main__':
+    # This is a large (almost 5MB) image
+    url = 'http://northwestcoast.ca/wp-content/uploads/2014/07/Matier-and-Stonecrop-Glacier-Joffre-Provincial-Park-large.jpg'
+
+    splits = [1, 4, 10, 16, 25, 50, 100]
+
+    loop = asyncio.get_event_loop()
+    tally = loop.run_until_complete(download_many(url, splits))
+
+    # aggregate tally
+    split_counters = {s: 0 for s in splits}
+
+    for _, dd in tally.items():
+        for _split, _elapsed in dd.items():
+            split_counters[_split] += _elapsed
+
+    for _split, _total_elapsed in split_counters.items():
+        final = float(_total_elapsed) / NUM_ITERATIONS
+        print('%d request splits resulted in average %2.4f sec over %d iterations' %
+              (_split, final, NUM_ITERATIONS))
